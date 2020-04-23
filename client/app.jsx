202a3836bd173f61e3c0a97137f613108ee177ad
Oskar Szura <oskar.szura@gmail.com>
2020-04-23 20:41:21 +0200

    CODA-68 - CODA_ website refurnishing

---
+++ client/app.jsx
@@ -0,0 +1,27 @@
+// @flow
+import React from "react";
+import { render } from "react-dom";
+import { Logo } from "graphen";
+
+const appContainer = document.querySelector(".js-app");
+
+type Props = {};
+
+function App(props: Props) {
+    return (
+        <>
+          <section className="l-splash">
+            <Logo />
+          </section>
+          <section className="gc-page">
+          </section>
+        </>
+    );
+}
+
+if (appContainer) {
+    render(
+      <App />,
+      appContainer
+    );
+}
