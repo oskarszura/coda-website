91d7719bef83576f149d00e50de2b8e05b587e2a
Oskar Szura <oskar.szura@gmail.com>
2020-04-23 21:03:12 +0200

    CODA-68 - CODA_ website refurnishing

--- client/app.jsx
+++ client/app.jsx
@@ -1,27 +1,23 @@
 // @flow
-import React from "react";
-import { render } from "react-dom";
-import { Logo } from "graphen";
+import React from 'react';
+import { render } from 'react-dom';
+import { Logo } from 'graphen';
 
-const appContainer = document.querySelector(".js-app");
+const appContainer = document.querySelector('.js-app');
 
 type Props = {};
 
 function App(props: Props) {
-    return (
-        <>
-          <section className="l-splash">
-            <Logo />
-          </section>
-          <section className="gc-page">
-          </section>
-        </>
-    );
+  return (
+    <>
+      <section className="l-splash">
+        <Logo className="l-logo" />
+      </section>
+      <section className="gc-page"></section>
+    </>
+  );
 }
 
 if (appContainer) {
-    render(
-      <App />,
-      appContainer
-    );
+  render(<App />, appContainer);
 }
