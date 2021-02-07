ff3bd4bc919e0abb54e2bba5ad253e962927cd7c
Oskar Szura <oskar.szura@gmail.com>
2021-02-07 09:29:25 +0100

    CODA-112 - Design corporate template (#7)

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Logo } from 'graphen';
+import Services from './modules/services';
 
 const appContainer = document.querySelector('.js-app');
 
@@ -11,8 +12,9 @@ function App(props: Props) {
   return (
     <>
       <section className="l-splash">
-        <Logo className="l-logo" />
+        <div className="l-splash__logo l-icon__logo" />
       </section>
+      <Services />
     </>
   );
 }
